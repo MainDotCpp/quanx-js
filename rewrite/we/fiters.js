b5f063de4a42e2137828dd06441869a7f8c1ad82
yangyang <1400002030@qq.com>
2022-11-03 09:33:32 +0800

    init

---
+++ rewrite/we/fiters.js
@@ -0,0 +1,14 @@
+// 重写响应
+const rewrite = (body) => {
+    let {data: {cpUserList}} = body
+    if (cpUserList == null) {
+        return
+    }
+    for (let cpUser of cpUserList) {
+        cpUser.nickname = `${cpUser.nickname}_CCCCCCCCCCCCCCCC`;
+    }
+}
+
+let body = JSON.parse($request.body)
+rewrite(body)
+$done({body: JSON.stringify(body)})
