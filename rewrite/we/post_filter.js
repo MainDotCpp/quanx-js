88380bcb9a0c4891177f231961d8ea14fded4b17
yangyang <1400002030@qq.com>
2024-02-13 09:14:23 +0800

    123

--- rewrite/we/post_filter.js
+++ rewrite/we/post_filter.js
@@ -1,4 +1,6 @@
 
+console.log($request)
+console.log(JSON.stringify($response))
 if (/bbs.post.newer.list/.test($request.body)){
   let body = JSON.parse($response.body)
   let obj = {...body,data:[]}
